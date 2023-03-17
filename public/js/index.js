db17e5c9c2a310fbe21bfefcc14213f4dbd547e1
Rik <ingeniero_rik@hotmail.com>
2023-03-17 20:55:17 -0300

    se agrego validacion de front con js para LOGIN

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,44 @@
-const navToggle = document.querySelector(".abrir-menu");
-const menu = document.querySelector(".menu");
+window.addEventListener('load', function() {
 
-navToggle.addEventListener("click", () => {
-  menu.classList.toggle("menu_visible");
-
-  if (menu.classList.contains("menu_visible")) {
-    navToggle.setAttribute("aria-label", "Cerrar menú");
-  } else {
-    navToggle.setAttribute("aria-label", "Abrir menú");
-  }
-});
+  const navToggle = document.querySelector(".abrir-menu");
+  const menu = document.querySelector(".menu");
+  
+  navToggle.addEventListener("click", () => {
+    menu.classList.toggle("menu_visible");
+  
+    if (menu.classList.contains("menu_visible")) {
+      navToggle.setAttribute("aria-label", "Cerrar menú");
+    } else {
+      navToggle.setAttribute("aria-label", "Abrir menú");
+    }
+  
+  })
+  
+  let botonEnvio = document.querySelector("#btn-envio");
+  let inputEmail =document.querySelector('#form-control');
+  let inputClave = document.querySelector("#clave-login")
+  let registerForm = document.querySelector(".form-login");
+  let errorNombre = document.querySelector(".errorNombre");
+  let errorClave = document.querySelector(".errorClave");
+  
+    botonEnvio.addEventListener('click', function(e){
+              e.preventDefault();
+              let errors = {}
+              if(inputEmail.value.length < 1){
+                  errors.email = 'Escriba su email';
+              }
+  
+              if (inputClave.value.length < 1){
+                errors.clave = "Escriba su contraseña";
+              }
+              if(Object.keys(errors).length >= 1){
+                  errorNombre.innerText = (errors.email) ? errors.email : ''; 
+                  errorClave.innerText = (errors.clave) ? errors.clave : '';
+              } else {
+              registerForm.submit();
+              }
+    })
+  
+  
+  
+  });
